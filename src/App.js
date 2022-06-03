bb90e323c5319c01b27c8cf721bcb1eb734364ff
tronngo <tron.ngo23@student.passerellesnumeriques.org>
2022-06-03 17:09:58 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-<<<<<<< Updated upstream
-=======
+
 import "./App.css";
 
 import FooterMain from "./components/footers/footerMain";
->>>>>>> Stashed changes
+
 
 import './App.css';
 // import Header from './Component/Header';
 import Menu from './Component/Menu';
 function App() {
   return (
-<<<<<<< Updated upstream
     <div className="App">
-      {/* <Header></Header> */}
+
       <Menu/>
       
-=======
+
     <div>
-      <FooterMain />
->>>>>>> Stashed changes
-    </div>
+      
+
   );
 }
 
