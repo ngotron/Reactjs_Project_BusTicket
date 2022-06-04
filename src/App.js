2b9e26b444b443ea70f6451c6f38bf45ba07ce68
tranvanhieu2002 <hieu.tran23@student.passerellesnumeriques.org>
2022-06-04 14:49:55 +0700

    change somwthing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 
-import "./App.css";
-
-import FooterMain from "./components/footers/footerMain";
 
 
-import './App.css';
+// import FooterMain from "./components/footers/footerMain";
+
+
 // import Header from './Component/Header';
 import Menu from './Component/Menu';
 function App() {
   return (
     <div className="App">
-
       <Menu/>
       
-
-    <div>
-      
-
+    </div>
+    
   );
 }
 
