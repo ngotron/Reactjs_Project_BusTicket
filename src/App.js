8ee036d6d45c7dbc1495316c785af4c249f48499
tranvanhieu01012002 <hieu.tran23@student.passerellesnumeriques.org>
2022-07-02 11:34:24 +0700

    remove error page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-import Error from "./Pages/Error";
 import WebVeXe from "./Pages/WebVeXe";
 import Find from "./Pages/Find";
 import FooterMain from "./Component/footer/footerMain";
@@ -17,8 +16,6 @@ function App() {
         <Menu/>
         <Routes>
             <Route path="/" element={<Home/>}></Route>
-            <Route path="/:string" element={<Error/>}></Route>
-            <Route path="/Error" element={<Error/>}></Route>
             <Route path="/Find" element={<Find/>}></Route>
             <Route path="/SendMail" element={<SendMail/>}></Route>
             <Route path="/Web/:id" element={<WebVeXe/>}></Route>
